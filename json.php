b96dfe24d003cb834bbf0c0c03a9d746b75bba46
NINJA <musab.mohamed206@gmail.com>
2020-12-07 11:51:32 +0300

    upload imap class

---
+++ json.php
@@ -0,0 +1,29 @@
+<?php
+
+// include Imap.Class
+include_once('lib/class.imap.php');
+
+$email = new Imap();
+$connect = $email->connect(
+	'{imap.gmail.com:993/imap/ssl/novalidate-cert}INBOX', //host
+	'xxxxxxxxxxxxx', //username
+	'xxxxxxxxxx' //password
+);
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+if($connect){
+	if(isset($_POST['inbox'])){
+		// inbox array
+		$inbox = $email->getMessages('html');
+		echo json_encode($inbox, JSON_PRETTY_PRINT);
+	}else if(!empty($_POST['uid']) && !empty($_POST['part']) && !empty($_POST['file']) && !empty($_POST['encoding'])){
+		// attachments
+		$inbox = $email->getFiles($_POST);
+		echo json_encode($inbox, JSON_PRETTY_PRINT);
+	}else {
+		echo json_encode(array("status" => "error", "message" => "Not connect."), JSON_PRETTY_PRINT);
+	}
+}else{
+	echo json_encode(array("status" => "error", "message" => "Not connect."), JSON_PRETTY_PRINT);
+}
